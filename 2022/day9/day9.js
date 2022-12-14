86bc8e10784078051268211380e0c76c8b1841b0
azrrik <azrrik@gmail.com>
2022-12-14 00:09:23 -0500

    day9 part1 done

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -1,137 +1,144 @@
-import assert from 'assert';
+import assert from "assert";
 
-import { fileToArrayOfLines, range } from '../util.js';
-
+import { fileToArrayOfLines, range } from "../util.js";
 
 const solve = (data) => {
-
-  // const moveTail = (pos, headPos) => {
-  //   if (pos.x === headPos.x) {
-  //     return (pos.y > headPos.y)
-  //       ? { x: pos.x, y: pos.y - 1 }
-  //       : { x: pos.x, y: pos.y + 1 }
-  //   }
-  //   if (pos.y === headPos.y) {
-  //     return (pos.x > headPos.x)
-  //       ? { x: pos.x - 1, y: pos.y }
-  //       : { x: pos.x + 1, y: pos.y }
-  //   }
-  //   if (pos.x - headPos.x === 1) {
-  //     if (pos.x > headPos.x) {
-  //       return (pos.y > headPos.y)
-  //         ? { x: pos.x - 1, y: pos.y - 1 }
-  //         : { x: pos.x + 1, y: pos.y + 1 }
-  //     } else {
-  //       return (pos.y > headPos.y)
-  //         ? { x: pos.x - 1, y: pos.y - 1 }
-  //         : { x: pos.x + 1, y: pos.y + 1 }
-  //     }
-  //   }
-  //   if (pos.y - headPos.y === 1) {
-  //     return (pos.x > headPos.x)
-  //       ? { x: pos.x - 1, y: pos.y - 1 }
-  //       : { x: pos.x + 1, y: pos.y + 1 }
-  //   }
-  //   throw 'invalid positions';
-  // }
-
-  // moveTail[headX - tailX][headY - tailY]
-  const moveTail =
-  {
-    '-1': {
-      '-1': {
-        // ..T
-        // .H.
-        // ...
-        dx: 1, dy: -1
-      },
-      '0': {
-
-      },
-      '1': {
-
-      }
+  // moveTail[head.x - tail.x][head.y - tail.y]
+  const moveTail = {
+    "-2": {
+      // ..T    ...
+      // H.. => HT.
+      "-1": { x: -1, y: -1 },
+      // H.T => HT.
+      0: { x: -1, y: 0 },
+      // H..    .TH
+      // ..T => ...
+      1: { x: -1, y: 1 },
     },
-
-    '0': {
-
+    "-1": {
+      // .T    ..
+      // ..    T.
+      // H. => H.
+      "-2": { x: -1, y: -1 },
+      // .T    .T
+      // H. => H.
+      "-1": { x: 0, y: 0 },
+      // HT => HT
+      0: { x: 0, y: 0 },
+      // H.    H.
+      // .T => .T
+      1: { x: 0, y: 0 },
+      // H.    H.
+      // ..    T.
+      // .T => ..
+      2: { x: -1, y: 1 },
     },
+    0: {
+      // T    .
+      // .    T
+      // H => H
+      "-2": { x: 0, y: -1 },
+      // T    T
+      // H => H
+      "-1": { x: 0, y: 0 },
+      0: { x: 0, y: 0 },
+      // H    H
+      // T => T
+      1: { x: 0, y: 0 },
+      // H    H
+      // .    T
+      // T => .
+      2: { x: 0, y: 1 },
+    },
+    1: {
+      // T.    ..
+      // ..    .T
+      // .H => .H
+      "-2": { x: 1, y: -1 },
+      // T.    T.
+      // .H => .H
+      "-1": { x: 0, y: 0 },
+      // TH => TH
+      0: { x: 0, y: 0 },
+      // .H    .H
+      // T. => T.
+      1: { x: 0, y: 0 },
+      // .H    .H
+      // ..    .T
+      // T. => ..
+      2: { x: 1, y: 1 },
+    },
+    2: {
+      // T..    ...
+      // ..H => .TH
+      "-1": { x: 1, y: -1 },
+      // T.H => .TH
+      0: { x: 1, y: 0 },
+      // ..H    .TH
+      // T.. => ...
+      1: { x: 1, y: 1 },
+    },
+  };
 
-    '1': {
-
-    }
-  }
-
-
-  const distance = (pos1, pos2) =>
-    Math.max(
-      Math.abs(pos1.x - pos2.x),
-      Math.abs(pos1.y - pos2.y)
-    )
-
-  const updateTail = (pos, headPos) =>
-    (distance(pos, headPos) >= 2)
-      ? moveTail(pos, headPos)
-      : pos
+  const updateTail = (tailPos, headPos) => {
+    const d = moveTail[headPos.x - tailPos.x][headPos.y - tailPos.y];
+    return { x: tailPos.x + d.x, y: tailPos.y + d.y };
+  };
 
   const moveHead = {
-    'U': (pos) => ({ x: pos.x, y: pos.y + 1 }),
-    'D': (pos) => ({ x: pos.x, y: pos.y - 1 }),
-    'L': (pos) => ({ x: pos.x + 1, y: pos.y }),
-    'R': (pos) => ({ x: pos.x - 1, y: pos.y }),
-  }
+    U: (pos) => ({ x: pos.x, y: pos.y + 1 }),
+    D: (pos) => ({ x: pos.x, y: pos.y - 1 }),
+    L: (pos) => ({ x: pos.x - 1, y: pos.y }),
+    R: (pos) => ({ x: pos.x + 1, y: pos.y }),
+  };
 
   const process = (acc, step) => {
-    console.log(acc, step)
-    range(1, step.length).reduce(
+    return range(1, step.length).reduce(
       (_acc) => {
-        const head = moveHead[step.direction](_acc.head)
-        console.log(head)
-        const tail = updateTail(_acc.tail, head)
-        console.log(tail)
-        return { head, tail }
+        const head = moveHead[step.direction](_acc.head);
+        const tail = updateTail(_acc.tail, head);
+        _acc.ground.add(`${tail.x},${tail.y}`);
+        return { head, tail, ground: _acc.ground };
       },
-      { head: acc.head, tail: acc.tail }
-    )
+      { ground: acc.ground, head: acc.head, tail: acc.tail }
+    );
+  };
 
-    return { ...acc, head: newPosition }
-  }
+  const parse = (split) => ({
+    direction: split[0],
+    length: parseInt(split[1]),
+  });
 
-  const parse = (split) =>
-    ({ direction: split[0], length: parseInt(split[1]) });
+  const acc = data
+    .map((line) => parse(line.split(" ")))
+    .reduce(process, {
+      ground: new Set(),
+      head: { x: 0, y: 0 },
+      tail: { x: 0, y: 0 },
+    });
+  const solution1 = acc.ground.size;
 
-  data
-    .map((line) => parse(line.split(' ')))
-    .reduce(
-      process,
-      {
-        ground: [[]],
-        head: { x: 0, y: 0 },
-        tail: { x: 0, y: 0 },
-      }
-    )
+  const solution2 = 0;
 
+  return { solution1, solution2 };
+};
 
-  const solution1 = 0
-  const solution2 = 0
-
-  return { solution1, solution2 }
-}
-
-
-const test = fileToArrayOfLines('./2022/day9/test.txt');
+const test = fileToArrayOfLines("./2022/day9/test.txt");
 const { solution1: testSolution1, solution2: testSolution2 } = solve(test);
 console.log(testSolution1);
-// assert.equal(testSolution1, '21', "total hidden trees should be 21");
+assert.equal(
+  testSolution1,
+  "13",
+  "total positions tail has visited should be 13"
+);
 
 // console.log(testSolution2);
 // assert.equal(testSolution2, '8', "total scenic score should be 8");
 
-
-// const input = fileToArrayOfLines('./2022/day8/input.txt');
-// const { solution1, solution2 } = solve(input);
-// console.log(solution1);
-// assert.equal(solution1, '1679', "total hidden trees should be 1679");
+const input = fileToArrayOfLines("./2022/day9/input.txt");
+const { solution1, solution2 } = solve(input);
+console.log(solution1);
+assert.equal(solution1, "1679", "total hidden trees should be 1679");
 
 // console.log(solution2);
 // assert.equal(solution2, '536625', "total scenic score should be 536625");
